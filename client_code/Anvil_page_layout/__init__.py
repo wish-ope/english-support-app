56d62a7c0f8782cc67e0ae8b96933cc35bc74df0
viet.210072@tbd.edu.vn <viet.210072@tbd.edu.vn>
2025-04-26 16:55:25 +0000

    Edited Anvil_page_layout

--- client_code/Anvil_page_layout/__init__.py
+++ client_code/Anvil_page_layout/__init__.py
@@ -38,6 +38,7 @@ class Anvil_page_layout(Anvil_page_layoutTemplate):
     self.curr_user = anvil.users.get_user()
     if self.curr_user is not None:
       self.show_user_bth()
+      open_form('Home_page')
     
     
   #function log out
